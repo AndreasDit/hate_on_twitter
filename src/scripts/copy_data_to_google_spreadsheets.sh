41196811f46c6b151f81febdae599a10d53d94a8
Andreas Ditte <Andreas.Ditte01@gmail.com>
2023-01-18 07:57:56 +0100

    Create script for copy job to google spreadsheets

---
+++ src/scripts/copy_data_to_google_spreadsheets.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo /opt/miniconda/bin/python3.7 /hate_on_twitter/src/google_spreadsheets/load_data_spreadsheet.py
+# sudo /opt/miniconda/bin/python3.7 /hate_on_twitter/src/main.py
+# sudo /opt/miniconda/bin/python3.7 /hate_on_twitter/src/main.py
+
+
+
